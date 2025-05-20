99489abba086e526ba72ec8c2c9980c2cedb6270
fabrice piedanna <rs45959@gmail.com>
2025-05-20 13:14:37 +0200

    update viteconfig for deployment

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,24 @@ export default {
     port: 8080,
     hot: true,
   },
+  base: "/MarioLand/",
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+        contact: path.resolve(__dirname, "src/contact.html"),
+        photos: path.resolve(__dirname, "src/photos.html"),
+        tarif: path.resolve(__dirname, "src/tarifs.html"),
+        mentionsLegales: path.resolve(__dirname, "src/mentions-legales.html"),
+        // Add other entry points if needed
+      },
     },
   },
 };
