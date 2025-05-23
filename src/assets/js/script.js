e6a64ca4598ae2a90fdc738b12c713f39b1e12a4
fabrice piedanna <rs45959@gmail.com>
2025-05-23 17:01:19 +0200

    Retrait de vite

--- src/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
-
-// or, specify which plugins you need:
-// import { Tooltip, Toast, Popover } from 'bootstrap';
-
-document.addEventListener("DOMContentLoaded", () => {
-  /* Commencez à coder du javascript entre ces balises */
-});
