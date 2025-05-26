5294ca630798f7ab65e5e3a32d6e1ff5705a3a37
Rodrigue Botte <rodrigue.botte@gmail.com>
2025-05-26 12:12:39 +0200

    mise en place de l'animation du header et du font pour la page tarif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  /* Commencez à coder du javascript entre ces balises */
-});
+  // Animation des Mario sortant des différents tuyaux
+  const marioPeace = document.querySelector(".marioPeace");
+  const marioMini = document.querySelector(".marioMini");
+  const marioDead = document.querySelector(".marioDead");
 
-// Animation des Mario sortant des différents tuyaux
-const marioPeace = document.querySelector('.marioPeace');
-const marioMini = document.querySelector('.marioMini');
-const marioDead = document.querySelector('.marioDead');
+  document.querySelector(".grandTuyau").addEventListener("mouseover", () => {
+    marioPeace.style.bottom = "30%";
+  });
+  document.querySelector(".grandTuyau").addEventListener("mouseleave", () => {
+    marioPeace.style.bottom = "14%";
+  });
 
-document.querySelector('.grandTuyau').addEventListener('mouseover', () => {
-  marioPeace.style.bottom = "30%";
-})
-document.querySelector('.grandTuyau').addEventListener('mouseleave', () => {
-  marioPeace.style.bottom = "14%";
-})
+  document.querySelector(".petitTuyau").addEventListener("mouseover", () => {
+    marioMini.style.bottom = "15%";
+  });
+  document.querySelector(".petitTuyau").addEventListener("mouseleave", () => {
+    marioMini.style.bottom = "0%";
+  });
 
-document.querySelector(".petitTuyau").addEventListener("mouseover", () => {
-  marioMini.style.bottom = "15%";
-});
-document.querySelector(".petitTuyau").addEventListener("mouseleave", () => {
-  marioMini.style.bottom = "0%";
-});
+  document.querySelector(".moyenTuyau").addEventListener("mouseover", () => {
+    marioDead.style.bottom = "25%";
+  });
+  document.querySelector(".moyenTuyau").addEventListener("mouseleave", () => {
+    marioDead.style.bottom = "10%";
+  });
 
-document.querySelector(".moyenTuyau").addEventListener("mouseover", () => {
-  marioDead.style.bottom = "25%";
-});
-document.querySelector(".moyenTuyau").addEventListener("mouseleave", () => {
-  marioDead.style.bottom = "10%";
-});
+  // animation de la bar de nav pour le menu burger
 
-// animation de la bar de nav pour le menu burger
+  const toggleBtn = document.getElementById("menu-toggle");
+  const nav = document.querySelector(".main-nav-mobile");
 
-const toggleBtn = document.getElementById("menu-toggle");
-const nav = document.querySelector(".main-nav-mobile");
+  toggleBtn.addEventListener("click", () => {
+    const isExpanded = toggleBtn.getAttribute("aria-expanded") === "true";
 
-toggleBtn.addEventListener("click", () => {
-  const isExpanded = toggleBtn.getAttribute("aria-expanded") === "true";
-
-  toggleBtn.setAttribute("aria-expanded", String(!isExpanded));
-  nav.hidden = isExpanded;
+    toggleBtn.setAttribute("aria-expanded", String(!isExpanded));
+    nav.hidden = isExpanded;
+  });
 });
