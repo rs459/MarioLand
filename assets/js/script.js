86cf5d7f18a0138ccea918b99fa6d003554ff610
Rodrigue Botte <rodrigue.botte@gmail.com>
2025-05-26 12:12:39 +0200

    mise en place de l'animation du header et du font pour la page tarif

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,27 @@ document.addEventListener("DOMContentLoaded", () => {
     nav.hidden = isExpanded;
   });
 });
+const marioPeace = document.querySelector('.marioPeace');
+const marioMini = document.querySelector('.marioMini');
+const marioDead = document.querySelector('.marioDead');
+
+document.querySelector('.grandTuyau').addEventListener('mouseover', () => {
+  marioPeace.style.bottom = "30%";
+})
+document.querySelector('.grandTuyau').addEventListener('mouseleave', () => {
+  marioPeace.style.bottom = "14%";
+})
+
+document.querySelector(".petitTuyau").addEventListener("mouseover", () => {
+  marioMini.style.bottom = "15%";
+});
+document.querySelector(".petitTuyau").addEventListener("mouseleave", () => {
+  marioMini.style.bottom = "0%";
+});
+
+document.querySelector(".moyenTuyau").addEventListener("mouseover", () => {
+  marioDead.style.bottom = "25%";
+});
+document.querySelector(".moyenTuyau").addEventListener("mouseleave", () => {
+  marioDead.style.bottom = "10%";
+});
