460303cbb80ea0897ef3a85b4f579e6aba5088e7
Rodrigue Botte <rodrigue.botte@gmail.com>
2025-05-27 11:32:04 +0200

    mise en place de la navbar du menu burger, ajustement de detail de la version mobile de la page tarifs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   /* Commencez à coder du javascript entre ces balises */
 });
+
+// Animation des Mario sortant des différents tuyaux
 const marioPeace = document.querySelector('.marioPeace');
 const marioMini = document.querySelector('.marioMini');
 const marioDead = document.querySelector('.marioDead');
@@ -24,4 +26,16 @@ document.querySelector(".moyenTuyau").addEventListener("mouseover", () => {
 });
 document.querySelector(".moyenTuyau").addEventListener("mouseleave", () => {
   marioDead.style.bottom = "10%";
+});
+
+// animation de la bar de nav pour le menu burger
+
+const toggleBtn = document.getElementById("menu-toggle");
+const nav = document.querySelector(".main-nav-mobile");
+
+toggleBtn.addEventListener("click", () => {
+  const isExpanded = toggleBtn.getAttribute("aria-expanded") === "true";
+
+  toggleBtn.setAttribute("aria-expanded", String(!isExpanded));
+  nav.hidden = isExpanded;
 });
